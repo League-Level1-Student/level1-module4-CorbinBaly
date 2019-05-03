24214cb0df4c7d56169edf02287a2c25bc4aabca
league <league@imac-8.attlocal.net>
2019-05-02 19:59:12 -0700

    Worked on calculator.

--- src/calculator.java
+++ src/calculator.java
@@ -2,58 +2,117 @@
 import java.awt.event.MouseEvent;
 import java.awt.event.MouseListener;
 
+import javax.swing.JButton;
 import javax.swing.JFrame;
 import javax.swing.JLabel;
 import javax.swing.JPanel;
+import javax.swing.JTextField;
 
 public class calculator implements MouseListener {
-public void makeframe() {
+	JPanel panel = new JPanel();
+	JButton add = new JButton();
+	JButton sub = new JButton();
+	JButton mul = new JButton();
+	JButton div = new JButton();
 	JFrame frame = new JFrame();
-	frame.setName("Simple Calculator");
-	JPanel addpanel = new JPanel();
-	addpanel.addMouseListener(this);
-	JPanel subtractpanel= new JPanel();
-	subtractpanel.addMouseListener(this);
-	JPanel multiplypanel = new JPanel();
-	multiplypanel.addMouseListener(this);
-	JPanel dividepanel = new JPanel();
-	dividepanel.addMouseListener(this);
-}
-public void add(){
+	JLabel result;
+	int resultnumber = 0;
+	JTextField fieldone;
+	JTextField fieldtwo;
+	int firstnumb;
+	int secondnumb;
 
-}
-public void subtract() {
+	public void makeframe() {
+		frame.setName("Simple Calculator");
+		frame.add(panel);
+		basicallyspamcode();
+		frame.pack();
+	}
 
-}
-public void multiply() {
+	public void basicallyspamcode() {
+		fieldone = new JTextField(9);
+		fieldtwo = new JTextField(9);
+		panel.add(result);
+		result.setText(text);
+		panel.add(fieldone);
+		panel.add(fieldtwo);
+		add.setText("add");
+		sub.setText("sub");
+		mul.setText("mul");
+		div.setText("div");
+		panel.add(add);
+		panel.add(sub);
+		panel.add(mul);
+		panel.add(div);
+		panel.add(result);
+		panel.setVisible(true);
+		frame.setVisible(true);
+		add.addMouseListener(this);
+		sub.addMouseListener(this);
+		mul.addMouseListener(this);
+		div.addMouseListener(this);
 
-}
-public void divide() {
+	}
 
+	public void add() {
+		resultnumber = firstnumb + secondnumb;
+	}
+
+	public void subtract() {
+		resultnumber = firstnumb - secondnumb;
+	}
+
+	public void multiply() {
+		resultnumber = firstnumb * secondnumb;
+	}
+
+	public void divide() {
+		resultnumber = firstnumb - secondnumb;
+	}
+
+	@Override
+	public void mouseClicked(MouseEvent e) {
+		// TODO Auto-generated method stub
+		JButton clicked = (JButton) e.getSource();
+		firstnumb = Integer.parseInt(fieldone.getText());
+		secondnumb = Integer.parseInt(fieldtwo.getText());
+		System.out.println(firstnumb);
+		System.out.println(secondnumb);
+if(clicked==add) {
+	add();
 }
-@Override
-public void mouseClicked(MouseEvent e) {
-	// TODO Auto-generated method stub
-	JPanel clicked = (JPanel) e.getSource();
+else if(clicked==sub) {
+	subtract();
 }
-@Override
-public void mousePressed(MouseEvent e) {
-	// TODO Auto-generated method stub
-	
+else if(clicked==mul) {
+	multiply();
 }
-@Override
-public void mouseReleased(MouseEvent e) {
-	// TODO Auto-generated method stub
-	
+else if(clicked==div) {
+	divide();
 }
-@Override
-public void mouseEntered(MouseEvent e) {
-	// TODO Auto-generated method stub
-	
-}
-@Override
-public void mouseExited(MouseEvent e) {
-	// TODO Auto-generated method stub
-	
-}
+	}
+
+	@Override
+	public void mousePressed(MouseEvent e) {
+		// TODO Auto-generated method stub
+
+	}
+
+	@Override
+	public void mouseReleased(MouseEvent e) {
+		// TODO Auto-generated method stub
+
+	}
+
+	@Override
+	public void mouseEntered(MouseEvent e) {
+		// TODO Auto-generated method stub
+
+	}
+
+	@Override
+	public void mouseExited(MouseEvent e) {
+		// TODO Auto-generated method stub
+
+	}
 }
