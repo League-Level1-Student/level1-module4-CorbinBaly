7c34ac903a6559d9e01d9674416041a156ed2e78
league <league@imac-8.attlocal.net>
2019-05-02 20:00:01 -0700

    Worked on calculator

--- src/calculator.java
+++ src/calculator.java
@@ -33,7 +33,8 @@ public class calculator implements MouseListener {
 		fieldone = new JTextField(9);
 		fieldtwo = new JTextField(9);
 		panel.add(result);
-		result.setText(text);
+		//result.setText(text);
+		//dfsdf
 		panel.add(fieldone);
 		panel.add(fieldtwo);
 		add.setText("add");
